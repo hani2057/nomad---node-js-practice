6aebefe75a474499d6f0192d21a1d56954a3189b
hani2057 <hanikim2057@gmail.com>
2023-12-06 21:57:47 +0900

    feat: implement router

--- src/server.js
+++ src/server.js
@@ -5,15 +5,33 @@ const PORT = 4000;
 
 // express 앱 만들기
 const app = express();
-
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  return res.send("Home");
-};
-
 app.use(logger);
-app.get("/", handleHome);
+
+// 라우터를 정의
+const globalRouter = express.Router();
+
+// 라우터 내부에서 사용할 컨트롤러를 정의
+const handleHome = (req, res) => res.send("Home");
+
+// request listening
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit user");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`Serve listening on port http://localhost:${PORT}`);
