aa87b1417583eec62fb2afade72795adb473c23e
hani2057 <hanikim2057@gmail.com>
2023-12-06 21:59:27 +0900

    refactor: modularize routers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -7,27 +10,6 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-// 라우터를 정의
-const globalRouter = express.Router();
-
-// 라우터 내부에서 사용할 컨트롤러를 정의
-const handleHome = (req, res) => res.send("Home");
-
-// request listening
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit user");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
