75001d3997510b6e1ffb4e3fc8a4d05eef12f08f
hani2057 <hanikim2057@gmail.com>
2023-12-14 21:55:03 +0900

    feat: show login or logout button using conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,10 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" }); // 렌더할 파일명은 띄어쓰기 없이 소문자로 작성
+const fakeUser = {
+  username: "hani",
+  loggedIn: false,
+};
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", fakeUser }); // 렌더할 파일명은 띄어쓰기 없이 소문자로 작성
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search video");
