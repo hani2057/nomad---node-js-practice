f79463fdadbb3e8d51bb6e9ae38b8893a6c652c2
hani2057 <hanikim2057@gmail.com>
2023-12-19 22:37:20 +0900

    feat: make form to edit video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,34 +1,45 @@
-export const trending = (req, res) => {
-  const videos = [
-    {
-      title: "First Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Second Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-    {
-      title: "Third Video",
-      rating: 5,
-      comments: 2,
-      createdAt: "2 minutes ago",
-      views: 59,
-      id: 1,
-    },
-  ];
-  return res.render("home", { pageTitle: "Home", videos });
-}; // 렌더할 파일명은 띄어쓰기 없이 소문자로 작성
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) =>
+  res.render("home", { pageTitle: "Home", videos }); // 렌더할 파일명은 띄어쓰기 없이 소문자로 작성
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = (req, res) => {
+  return res.end();
+};
 export const search = (req, res) => res.send("Search video");
 export const upload = (req, res) => res.send("Upload video");
 export const deleteVideo = (req, res) => res.send("Delete video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { upload, see, edit, deleteVideo } from "../controllers/videoController";
+import {
+  upload,
+  watch,
+  deleteVideo,
+  getEdit,
+  postEdit,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("upload", upload);
-videoRouter.get("/:id(\\d+)", see); // \d+ 부분은 정규표현식(숫자-digit-만)
-videoRouter.get("/:id/edit", edit);
+videoRouter.get("/:id(\\d+)", watch); // \d+ 부분은 정규표현식(숫자-digit-만)
+videoRouter.get("/:id/edit", getEdit);
+videoRouter.post("/:id/edit", postEdit);
 videoRouter.get("/:id/delete", deleteVideo);
 
 export default videoRouter;
