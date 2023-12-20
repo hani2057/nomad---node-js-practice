fe0a0baf8516ce7b49f4f94a826bdd5a903835fe
hani2057 <hanikim2057@gmail.com>
2023-12-20 23:33:40 +0900

    feat: (recap) upload video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,22 +27,42 @@ let videos = [
 
 export const trending = (req, res) =>
   res.render("home", { pageTitle: "Home", videos }); // 렌더할 파일명은 띄어쓰기 없이 소문자로 작성
+
 export const watch = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
+
 export const getEdit = (req, res) => {
   const { id } = req.params;
   const video = videos[id - 1];
   return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
+
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
   videos[id - 1].title = title;
   return res.redirect(`/videos/${id}`);
 };
-export const search = (req, res) => res.send("Search video");
-export const upload = (req, res) => res.send("Upload video");
-export const deleteVideo = (req, res) => res.send("Delete video");
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    vidws: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,17 +1,16 @@
 import express from "express";
 import {
-  upload,
   watch,
-  deleteVideo,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("upload", upload);
 videoRouter.get("/:id(\\d+)", watch); // \d+ 부분은 정규표현식(숫자-digit-만)
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-videoRouter.get("/:id/delete", deleteVideo);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
